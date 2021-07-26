558c468ac88ff40cb817a600bc08fc0c9ee1ba78
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2021-07-26 12:42:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    if (ab == 1) {
+    if (_12 == 1) {
         punkty += 1
-        _12 = ab[randint(1, 2)]
-        if (ab == 1) {
-            music.playMelody("C5 B A G F E D C ", 120)
-        } else if (ab == 2) {
-            music.playMelody("C D E F G A B C5 ", 120)
+        _12 = lista[randint(1, 2)]
+        if (_12 == 1) {
+            music.ringTone(147)
+        } else if (_12 == 2) {
+            music.ringTone(147)
+            music.ringTone(147)
         }
-    } else if (ab == 2) {
+    } else if (_12 == 2) {
         życie += -1
-        _12 = ab[randint(1, 2)]
-        if (ab == 1) {
-            music.playMelody("C5 B A G F E D C ", 120)
-        } else if (ab == 2) {
-            music.playMelody("C D E F G A B C5 ", 120)
+        _12 = lista[randint(1, 2)]
+        if (_12 == 1) {
+            music.ringTone(147)
+        } else if (_12 == 2) {
+            music.ringTone(147)
+            music.ringTone(147)
         }
     }
 })
@@ -21,32 +23,36 @@ input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
-    if (ab == 1) {
+    let ab = 0
+    if (_12 == 1) {
         punkty += -1
-        _12 = ab[randint(1, 2)]
-        if (ab == 1) {
-            music.playMelody("C5 B A G F E D C ", 120)
-        } else if (ab == 2) {
-            music.playMelody("C D E F G A B C5 ", 120)
+        _12 = lista[randint(1, 2)]
+        if (_12 == 1) {
+            music.ringTone(147)
+        } else if (_12 == 2) {
+            music.ringTone(147)
+            music.ringTone(147)
         }
     } else if (ab == 2) {
         życie += 1
-        _12 = ab[randint(1, 2)]
-        if (ab == 1) {
-            music.playMelody("C5 B A G F E D C ", 120)
-        } else if (ab == 2) {
-            music.playMelody("C D E F G A B C5 ", 120)
+        _12 = lista[randint(1, 2)]
+        if (_12 == 1) {
+            music.ringTone(147)
+        } else if (_12 == 2) {
+            music.ringTone(147)
+            music.ringTone(147)
         }
     }
 })
 let _12 = 0
-let ab = 0
+let lista: number[] = []
 let życie = 12
 let punkty = 0
-ab = ["1", "2"]
-_12 = ab[randint(1, 2)]
-if (ab == 1) {
-    music.playMelody("C5 B A G F E D C ", 120)
-} else if (ab == 2) {
-    music.playMelody("C D E F G A B C5 ", 120)
+lista = [1, 2]
+_12 = lista[randint(1, 2)]
+if (_12 == 1) {
+    music.ringTone(147)
+} else if (_12 == 2) {
+    music.ringTone(147)
+    music.ringTone(147)
 }
